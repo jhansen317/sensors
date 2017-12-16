471c4da177ba99f43c3d815ed2b2a2cbb7b0fec9
Jakob Hansen <jakob@soundwall.com>
2017-12-15 23:49:06 -0700

    simple web page with chart

--- server/export/apps/rest_api/api.py
+++ server/export/apps/rest_api/api.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, g
 import json
 import logging
 from datetime import date, datetime
+import time
 from pymongo import MongoClient
 
 logging.basicConfig(level=logging.DEBUG)
@@ -9,25 +10,31 @@ LOGGER = logging.getLogger(__name__)
 
 def json_serial(obj):
     """JSON serializer for objects not serializable by default json code"""
+
     if isinstance(obj, (datetime, date)):
-        return obj.isoformat()
+        return int(time.mktime(obj.timetuple())) * 1000
     raise TypeError ("Type %s not serializable" % type(obj))
 
 app = Flask(__name__)
 
+def get_mongo_db():
+    if not hasattr(g, 'mongo_db'):
+        g.mongo_db = MongoClient('localhost', username='www', password='webmongo', authSource='sensors').sensors
+    return g.mongo_db
+
+
 @app.route("/sensors/environment")
 def environment():
-    mongoc = MongoClient('localhost', username='www', password='webmongo', authSource='sensors')
-    mongo_sensors = mongoc.sensors
-    data = [d for d in mongo_sensors.environment.find(projection={'_id':False})]
+    mdb = get_mongo_db()
+    data = [d for d in mdb.environment.find(projection={'_id':False})]
     resp = json.dumps(data, default=json_serial)
     return (resp, 200, {'Content-type': 'application/json'})
 
 @app.route("/sensors/environment/limit/<int:records>")
 def environment_limit(records):
-    mongoc = MongoClient('localhost', username='www', password='webmongo', authSource='sensors')
-    mongo_sensors = mongoc.sensors
-    data = [d for d in mongo_sensors.environment.find(sort=[('_id', -1)], limit=records, projection={'_id':False})]
+    mdb = get_mongo_db()
+    data = [d for d in mdb.environment.find(sort=[('_id', -1)], limit=records, projection={'_id':False})]
+    data.reverse()
     resp = json.dumps(data, default=json_serial)
     return (resp, 200, {'Content-type': 'application/json'})
 
